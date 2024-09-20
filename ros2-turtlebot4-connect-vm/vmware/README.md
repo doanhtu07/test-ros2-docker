20910ff96ade3f1278ce69fb060f95cf3a060424
doanhtu07 <doanhtu07@gmail.com>
2024-09-19 20:42:43 -0500

    Add some scripts for setting up turtlebot4

---
+++ ros2-turtlebot4-connect-vm/vmware/README.md
@@ -0,0 +1,13 @@
+# How to use this Vagrantfile
+
+1. Install Vagrant
+
+2. Follow this and install necessary dependencies for Vagrant and VMWare Fusion
+
+- https://gist.github.com/manojkarthick/518ceb4569156ce2c95329b71f676bf0
+
+# VMWare Notes
+
+- Use Ctrl+Insert or Ctrl+Shift+C for copying and Shift+Insert or Ctrl+Shift+V for pasting text in the terminal in Ubuntu
+
+- Set Network Adapter to bridged network on Wi-Fi
---
+++ ros2-turtlebot4-connect-vm/vmware/src/setup-scripts/setup-turtlebot4.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Discovery server manual: https://turtlebot.github.io/turtlebot4-user-manual/setup/discovery_server.html
+
+# Base IP (first three octets)
+base_ip="192.168.0."
+
+# Starting and ending values for the last octet
+base=220
+start=1
+end=20
+finalInput="\n" # ROS_DOMAIN_ID set to 0 by default
+
+# Loop to increment the last octet
+for ((i=start; i<=end; i++)); do
+    # Construct the full IP address
+    sum=$((i+base))
+    full_ip="${base_ip}${sum}"
+
+    finalInput="${finalInput}${i}\n" # Server ID
+    finalInput="${finalInput}${full_ip}\n" # Server IP
+    finalInput="${finalInput}\n" # Server port set to 11311 by default
+
+    if [ $i -eq $end ]; then
+        finalInput="${finalInput}d\n" # done
+    else
+        finalInput="${finalInput}a\n" # add more
+    fi
+done
+
+printf "$finalInput" | bash <(wget -qO - https://raw.githubusercontent.com/turtlebot/turtlebot4_setup/humble/turtlebot4_discovery/configure_discovery.sh)
+
+source /home/vagrant/.bashrc
+
+ros2 daemon stop; ros2 daemon start
